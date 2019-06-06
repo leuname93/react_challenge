16ebebe15eb1ec0c47312cccf123b65d9c9fa909
Emmanuel <esantiagohz@gmail.com>
2019-06-06 05:27:01 -0500

    agrego rutas

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux'
+import { combineReducers, createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import giphyBuilderReducer from './store/reducers/giphyBuilderReducer'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const reducer = combineReducers({ giphyReducer: giphyBuilderReducer() })
+
+const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(thunk))
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root') 
+)
+
+
+
